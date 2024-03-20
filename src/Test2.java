f75e2ba75c836a6217966fbe84b85dfd5394907c
Sarita <sarita123@gmail.com>
2024-03-20 21:35:11 +0100

    BaseProject Created

---
+++ src/Test2.java
@@ -0,0 +1,7 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test2");
+        System.out.println("Test222");
+        System.out.println("Test2222");
+    }
+}
